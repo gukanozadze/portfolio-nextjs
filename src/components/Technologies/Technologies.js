40a89c0e987090d416190f0dfba51f34c9035fff
Guka Nozadze <gukanozadze@gmail.com>
2022-03-03 21:07:56 +0400

    updated description

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -9,7 +9,7 @@ const Technologies = () => (
     <br />
     <SectionTitle>Technologies</SectionTitle>
       <SectionText>I know many libraries and design patterns. component libraries (Like MaterialUI, HeadlessUI, ChakraUI...), extensively used TailwindCSS, used either react-hook-form or Formik to manage forms that have values fetched from API.  </SectionText>
-      <SectionText>Developed dashboard interfaces , auth and payment pages and services, and much more..</SectionText>
+      <SectionText>Developed dashboard interfaces, multi-language auth and payment pages and services, and much more..</SectionText>
       <List>
       <ListItem>
         <DiReact size="3rem" />
